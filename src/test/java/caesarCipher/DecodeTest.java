8775cee68433c6e297e4af9588c9dcf046aa380c
Fabienne Hirwa <fabiennehirwa1@gmail.com>
2020-11-16 12:25:24 -0500

    edit decodeTest class

--- src/test/java/caesarCipher/DecodeTest.java
+++ src/test/java/caesarCipher/DecodeTest.java
@@ -1,7 +1,16 @@
 package caesarCipher;
 
+import org.junit.Test;
 import static org.junit.Assert.*;
 
+
+
+
 public class DecodeTest {
+    @Test
+    public void newEncode_getsPlain_2() {
+        Decode testEncode = new Decode("hello", 4);
+        assertEquals(true, testEncode instanceof Decode);
+    }
 
 }
