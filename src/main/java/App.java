b8793ca766bc073696e34d373a04216d9f3e770a
Fabienne Hirwa <fabiennehirwa1@gmail.com>
2020-11-16 12:28:06 -0500

    edit App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,29 @@
+
+import caesarCipher.Decode;
+import caesarCipher.Encode;
+
+import java.io.Console;
+
 public class App {
+    public static void main(String[] args) {
+
+        Console myConsole = System.console();
+        System.out.println("Enter the word you want to encrypt:");
+        String plainText = myConsole.readLine();
+        System.out.println("Enter number you want to be shifted: ");
+        String theKey = myConsole.readLine();
+        Integer key  = Integer.parseInt(theKey);
+        System.out.println("Encoded text: " + Encode.encrypt(plainText,key));
+        System.out.println("Decoded text: " + Decode.decrypt(Encode.encrypt(plainText,key),key));
+
+
+
+
+
+
+    }
+
+
+
 }
+
