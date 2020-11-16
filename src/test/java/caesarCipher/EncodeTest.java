047c62c4950e0b26526d019ba4b0a27db86b3e35
Fabienne Hirwa <fabiennehirwa1@gmail.com>
2020-11-16 12:24:26 -0500

    edit encodeTest class

--- src/test/java/caesarCipher/EncodeTest.java
+++ src/test/java/caesarCipher/EncodeTest.java
@@ -1,7 +1,23 @@
 package caesarCipher;
 
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class EncodeTest {
+    @Test
+    public void newRectangle_instantiatesCorrectly() {
+        Encode testEncode = new Encode("hello", 4);
+        assertEquals(true, testEncode instanceof Encode);
+    }
+    @Test
+    public void newEncode_getsPlain_2() {
+        Encode testEncode = new Encode("hello2", 4);
+        assertEquals("faby", testEncode.getPlain());
+    }
+    @Test
+    public void getEncode_getsmyKey() {
+        Encode testEncrypt = new Encode("hirwa", 2);
+        assertEquals(2, testEncrypt.getMyKey());
+    }
 
 }
